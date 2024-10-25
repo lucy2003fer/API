d13c94548f58e462c2b8597aa2fe8f2d870ec766
lucy2003fer <lucy48fernada@gmail.com>
2024-10-25 08:30:25 -0500

    creé el archivo requeriments e implementamos el jwt, funciona correctamente

--- blog/urls.py
+++ blog/urls.py
@@ -36,6 +36,9 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router_post.urls)),
+    path('api/', include('apps.user.api.router')),
+    
+    
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
